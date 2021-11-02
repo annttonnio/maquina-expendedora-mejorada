20d2e2a02ac5fd1b9ec345ebb8df252817c30f08
annttonnio <antonio56.lop@gmail.com>
2021-11-02 11:16:07 +0100

    0745.01 cantidadDeDineroQueFalta

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -13,10 +13,12 @@ public class MaquinaExpendedoraMejorada {
 
     /**
      * Crea una maquina expendedora de billetes de tren con el 
-     * precio del billete y el origen y destino dados. Se asume que el precio
+     * precio del billete y el origen y destino dados. 
+     * Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraMejorada(int precioDelBillete, String origen, String destino) {
+    public MaquinaExpendedoraMejorada(int precioDelBillete, 
+                    String origen, String destino) {
         precioBillete = precioDelBillete;
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
@@ -32,21 +34,25 @@ public class MaquinaExpendedoraMejorada {
     }
 
     /**
-     * Devuelve la cantidad de dinero que el cliente actual lleva introducida
+     * Devuelve la cantidad de dinero que el cliente 
+     * actual lleva introducida
      */
     public int getBalanceClienteActual() {
         return balanceClienteActual;
     }
 
     /**
-     * Simula la introduccion de dinero por parte del cliente actual
+     * Simula la introduccion de dinero por parte del 
+     * cliente actual
      */
     public void introducirDinero(int cantidadIntroducida) {
         if (cantidadIntroducida > 0) {
-            balanceClienteActual = balanceClienteActual + cantidadIntroducida;
+            balanceClienteActual = balanceClienteActual 
+                                    + cantidadIntroducida;
         }
         else {
-            System.out.println(cantidadIntroducida + " no es una cantidad de dinero valida.");
+            System.out.println(cantidadIntroducida 
+                    + " no es una cantidad de dinero valida.");
         }        
     }
 
@@ -54,29 +60,36 @@ public class MaquinaExpendedoraMejorada {
      * Imprime un billete para el cliente actual
      */
     public void imprimirBillete() {
-        if (balanceClienteActual >= precioBillete) {        
+        int cantidadDeDineroQueFalta = precioBillete - balanceClienteActual;
+        if (balanceClienteActual >= precioBillete) {            
             // Simula la impresion de un billete
             System.out.println("##################");
             System.out.println("# Billete de tren:");
-            System.out.println("# De " + estacionOrigen + " a " + estacionDestino);
+            System.out.println("# De " + estacionOrigen + " a " 
+                                            + estacionDestino);
             System.out.println("# " + precioBillete + " euros.");
             System.out.println("##################");
             System.out.println();         
     
             // Actualiza el total de dinero acumulado en la maquina
-            totalDineroAcumulado = totalDineroAcumulado + precioBillete;
+            totalDineroAcumulado = totalDineroAcumulado 
+                                            + precioBillete;
             // Reduce el balance del cliente actual dejandole seguir utilizando la maquina
-            balanceClienteActual = balanceClienteActual - precioBillete;
+            balanceClienteActual = balanceClienteActual 
+                                            - precioBillete;
         }
         else {
-            System.out.println("Necesitas introducir " + (precioBillete - balanceClienteActual) + " euros mas!");
+            System.out.println("Necesitas introducir " 
+                        + (cantidadDeDineroQueFalta)   
+                    + " euros mas!");
                     
-        }            
+        }        
     }
     
     /**
      * Cancela la operacion de compra del cliente actual y le
-     * devuelve al cliente el dinero que ha introducido hasta el momento
+     * devuelve al cliente el dinero que ha introducido 
+     * hasta el momento
      */
     public int cancelarOperacionYDevolverDinero() {
         int cantidadDeDineroADevolver;
